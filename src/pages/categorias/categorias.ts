06a197996fd0a207030e544c351423a776daf024
Edvaldo Segundo <edvaldo.segundo@hotmail.com>
2020-10-16 22:03:31 +0100

    Criando um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -24,9 +24,7 @@ export class CategoriasPage {
     this.categoriaServie.findAll().subscribe(response => {
       this.items = response;
     },
-      error => {
-        console.log(error);
-      });
+      error => { });
   }
 
 }
